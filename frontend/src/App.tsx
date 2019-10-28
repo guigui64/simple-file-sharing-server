09a79e636b823ba0f5ae368b3823d3abee777da4
guigui64 <guillaume.comte10@gmail.com>
2019-10-28 01:16:02 +0100

    add frontend in repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,138 @@
+import { faFileArchive } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, Input, Modal, Spinner } from 'react-rainbow-components';
+import './App.css';
+import logo from './logo.svg';
+
+const bcrypt = require('bcryptjs');
+
+const API_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:9999'
+    : 'http://gcomte.ddns.net:31415';
+
+type File = { id: string; name: string; path: string };
+
+const validatePwd = async (
+  file: File,
+  password: string,
+  success: () => void
+) => {
+  const hash = await bcrypt.hash(password, 10);
+  const res = await fetch(`${API_URL}/file?id=${file.id}&pwdHash=${hash}`);
+  if (res.ok) {
+    const link = document.createElement('a');
+    link.href = `${API_URL}/file?id=${file.id}&pwdHash=${hash}`;
+    link.setAttribute('download', `${file.path}`);
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode!.removeChild(link);
+    success();
+  } else {
+    const { reason } = await res.json();
+    alert(reason);
+  }
+};
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [files, setFiles] = useState([] as File[]);
+  useEffect(() => {
+    fetch(`${API_URL}/files`)
+      .then(res => res.json())
+      .then(files => setFiles(files))
+      .catch(err => setErrorMessage(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedFileId, setSelectedFileId] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Download your files here{' '}
+          <span role='img' aria-label='wink emoji'>
+            😉
+          </span>
+        </p>
+      </header>
+      {loading && <Spinner size='large' />}
+      {errorMessage && (
+        <p>{`Impossible to get the files from server. Error : ${errorMessage}`}</p>
+      )}
+      {!loading && !errorMessage && (
+        <div className='App-content'>
+          {files.map((f: any) => (
+            <Card
+              key={f.id}
+              title={f.name}
+              icon={
+                <FontAwesomeIcon
+                  icon={faFileArchive}
+                  size='lg'
+                  className='rainbow-color_success'
+                />
+              }
+              actions={
+                <Button
+                  label='Download'
+                  variant='success'
+                  onClick={() => {
+                    setSelectedFileId(f.id);
+                    setIsOpen(true);
+                  }}
+                />
+              }
+              style={{ margin: '10px 10px 0 10px' }}
+            />
+          ))}
+        </div>
+      )}
+      <Modal id='modal' isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+        <Input
+          label={`Enter password for file '${
+            selectedFileId === ''
+              ? 'NO_SELECTION'
+              : files.find(f => f.id === selectedFileId)!.name
+          }'`}
+          placeholder='Password'
+          type='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '5px'
+          }}
+        >
+          <Button
+            label='Download'
+            variant='success'
+            disabled={password === ''}
+            onClick={() => {
+              validatePwd(
+                files.find(f => f.id === selectedFileId)!,
+                password,
+                () => {
+                  setIsOpen(false);
+                  setPassword('');
+                  setSelectedFileId('');
+                }
+              );
+            }}
+          />
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default App;
